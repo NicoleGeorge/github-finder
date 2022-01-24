c06da9efa244d8e08cb660942ed4eafa5404b156
Nicole George <hello@nicolegeorge.me>
2022-01-24 14:52:19 +1030

    cleaning up install files, changing theme to dark

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    
-      <div className="bg-purple-500">
-        <h1 className="text-xl">hello</h1>
-
-        <button className="btn">click</button>
-      </div>
-
+    <div >
+      <h1 className='text-xl'>hello</h1>
+    </div>
   );
 }
 
