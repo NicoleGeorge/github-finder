c625b7c6595a5b9356510838a46b68e45195cb1c
Nicole George <hello@nicolegeorge.me>
2022-02-10 15:39:44 +1030

    adding user search functionality

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
       <Router>
         <div className='flex flex-col justify-between h-screen'>
           <Navbar />
-
           <main className='container mx-auto px-3 pb-12'>
             <Routes>
               <Route path='/' element={<Home />} />
